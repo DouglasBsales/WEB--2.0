b719b88d90fbcfc021ac422de6bd015fbf6f12fe
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2023-12-08 19:23:23 -0300

    att nav bar mobile
    adicionando ícone de fechamento do menu

--- Projeto6-Museu-nacional/script.js
+++ Projeto6-Museu-nacional/script.js
@@ -2,7 +2,10 @@ function menuShow(){
     let menuMobile = document.querySelector('.mobile-menu')
     if (menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
+        document.querySelector('.icon1').src = "imagens/menu.png"
+
     } else{
         menuMobile.classList.add('open')
+        document.querySelector('.icon1').src = "imagens/close.png"
     }
 }
