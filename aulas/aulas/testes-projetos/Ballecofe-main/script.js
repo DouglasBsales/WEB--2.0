052d2803d932ef68009e91cdc6b8149ea823e6cb
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-01-11 00:04:46 -0300

    fist commit

---
+++ aulas/aulas/testes-projetos/Ballecofe-main/script.js
@@ -0,0 +1,20 @@
+function buttonShow(){
+    let showButton = document.querySelector('.container-receitas');
+    if (showButton.classList.contains('open')){
+        showButton.classList.remove('open');
+    }
+    else {
+        (showButton.classList.add('open'))
+    }
+}
+
+function menuShow(){
+    showMenu = document.querySelector('.menu-mobile');
+        if(showMenu.classList.contains('open')){
+            showMenu.classList.remove('open')
+            document.querySelector('.icone').src = ('imagens/menu.svg');
+        } 
+        else {(showMenu.classList.add('open'))
+        document.querySelector('.icone').src = ('imagens/x.svg');
+    }
+}
