2c4971c0d44c14a6799cf6382284ee6b7e733838
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-01-18 22:44:49 -0300

    att exercicio logica

---
+++ javascript/exercicio/logica.js
@@ -0,0 +1,14 @@
+let idade = 70;
+
+if(idade <= 15){
+    console.log("Criança")
+}
+else if(idade <= 30){
+    console.log("Jovem")
+}
+else if(idade <= 60){
+    console.log("adulto")
+}
+else{
+    console.log("Idoso")
+};
--- javascript/exercicios_logica/exercicio9.js
+++ javascript/exercicios_logica/exercicio9.js
@@ -3,6 +3,23 @@ Esse algoritimo calcula o imc do usuario e diz em quais condicoes
 ele se encontra
 */
 
-let peso = 0;
-let altura = 0;
+let peso = 100;
+let altura = 1.80;
 let imc = peso / (altura * 2);
+console.log(imc.toFixed(1));
+
+if(imc <= 18.5){
+    console.log("Abaixo do peso")
+}
+else if(imc <= 24.9){
+    console.log("levemente a cima do peso")
+}
+else if(imc <= 34.9){
+    console.log("Obesidade grau I ")
+}
+else if(imc <= 39.9){
+    console.log("Obesidade grau II (severa)")
+}
+else{
+    console.log("Obesidade grau III (mórbida)")
+};
