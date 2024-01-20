9dd62dc51fe0f0ce9ea80a6060baf9f523f03646
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-01-19 22:12:48 -0300

    aula switch

---
+++ javascript/switch/switch.js
@@ -0,0 +1,14 @@
+let fruta = "caja";
+
+switch (fruta) {
+  case "morango":
+    console.log("Não temos morango no momento");
+    break;
+
+  case "maca":
+    console.log("Temos sim maça, quanto vai querer?");
+    break;
+
+  default:
+    console.log("Não temo essa fruta no momento");
+}
