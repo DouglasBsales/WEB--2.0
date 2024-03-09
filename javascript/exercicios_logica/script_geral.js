7207c37d403e53ab83b5196e837fc004b0591485
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-03-09 15:08:43 -0300

    att script of addUsers utilized maniulation of arrays

--- javascript/exercicios_logica/script_geral.js
+++ javascript/exercicios_logica/script_geral.js
@@ -1,1 +1,6 @@
-
+let numeroPassado = 6;
+if (numeroPassado % 2 === 0) {
+  console.log(numeroPassado + ":  esse número é par");
+} else {
+  console.log(numeroPassado + ":  esse número não é par");
+}
