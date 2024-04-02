5101acca45a6b06d270ce565da483bddf28d0e2c
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-04-02 20:09:32 -0300

    att teste scripts

--- javascript/projectsTeste/apiMockada/script.js
+++ javascript/projectsTeste/apiMockada/script.js
@@ -10,7 +10,6 @@ async function getComments() {
     method: "GET",
   });
   const data = await response.json();
-  console.log(data);
 
   divCarregando.classList.add("hidden");
 
@@ -18,6 +17,7 @@ async function getComments() {
     createComments(elemento);
   });
 }
+
 
 async function sendComment() {
   let comment = {
@@ -36,20 +36,32 @@ async function sendComment() {
 
   const data = response.json();
 
-  console.log(data);
 }
 
-divForm.addEventListener('submit', sendComment)
+async function deleteComments(id) {
+  const response = await fetch(`http://localhost:3000/comments/${id}`, {
+    method: "DELETE",
+  });
+
+}
 
 async function createComments(elemento) {
   const comments = document.createElement("div");
   comments.innerHTML = `
-    <div class="w-[400px] pb-[20px]">
-    <div><p class="text-2xl font-semibold">${elemento.name}</p>
-    <p class="text-lg font-semibold">${elemento.email}</p>
-    <p class="pt-[10px]" >${elemento.body}</p>
-    </div>
-    </div>
+      <div class="flex flex-col pb-[20px]">
+      <div>
+        <p class="text-2xl font-semibold">${elemento.name}</p>
+      </div>
+      <div class="w-[500px] flex justify-between">
+      <p class="text-lg font-semibold">${elemento.email}</p>
+      <button onclick="deleteComments('${elemento.id}')">
+      <i class="bi bi-trash text-[#919191] text-lg cursor-pointer"></i>
+      </button>
+      </div>
+      <div class="w-[400px]">
+      <p class="pt-[10px]" >${elemento.body}</p>
+      </div>
+      </div>
     `;
   divComments.appendChild(comments);
 }
